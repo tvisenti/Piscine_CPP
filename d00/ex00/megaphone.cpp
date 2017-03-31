f95356556a054531cf39374217000a951e878939
tvisenti <tvisenti@student.42.fr>
2017-03-31 12:02:44 +0200

    [ex00]: Use for instead of while

--- d00/ex00/megaphone.cpp
+++ d00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/29 11:48:18 by tvisenti          #+#    #+#             */
-/*   Updated: 2017/03/29 14:34:42 by tvisenti         ###   ########.fr       */
+/*   Updated: 2017/03/31 11:40:05 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,8 @@ int main(int argc, char **argv) {
 	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	} else {
-		int i = 0;
-		while (++i < argc) {
-			int cur = -1;
-			while (argv[i][++cur]) {
+		for (int i = 1; i < argc; i++) {
+			for (int cur = 0; argv[i][cur]; cur++) {
 				std::cout << (char)toupper(argv[i][cur]);
 			}
 		}
