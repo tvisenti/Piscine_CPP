9d657bf20e5a72310243170b61db5565a2b792fc
tvisenti <tvisenti@student.42.fr>
2017-04-04 19:33:32 +0200

    [ex08]: Tab pointer of func

---
+++ d01/ex08/Human.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Human.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/04 17:08:41 by tvisenti          #+#    #+#             */
+/*   Updated: 2017/04/04 19:26:12 by tvisenti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Human.hpp"
+
+void	Human::meleeAttack(std::string const & target) {
+	std::cout << "meleeAttack" << std::endl;
+	return;
+}
+
+void	Human::rangedAttack(std::string const & target) {
+	std::cout << "rangedAttack" << std::endl;
+	return;
+}
+
+void	Human::intimidatingShout(std::string const & target) {
+	std::cout << "intimidatingShout" << std::endl;
+	return;
+}
+
+void	Human::action(std::string const & action_name, std::string const & target) {
+	void	(Human::*array[3])(std::string const & target) =
+	{&Human::meleeAttack, &Human::rangedAttack, &Human::intimidatingShout};
+	std::string	cmp[3] = {"meleeAttack", "rangedAttack", "intimidatingShout"};
+
+	for (int i = 0; i < 3; i++) {
+		if (cmp[i] == action_name) {
+			this->(Human::*array[i])(target);
+			return;
+		}
+	}
+	return;
+}
---
+++ d01/ex08/Human.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Human.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/04 17:08:39 by tvisenti          #+#    #+#             */
+/*   Updated: 2017/04/04 17:35:36 by tvisenti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HUMAN_HPP
+# define HUMAN_HPP
+# include <iostream>
+class Human {
+	private:
+		void meleeAttack(std::string const & target);
+		void rangedAttack(std::string const & target);
+		void intimidatingShout(std::string const & target);
+
+	public:
+		void action(std::string const & action_name, std::string const & target);
+};
+
+#endif
