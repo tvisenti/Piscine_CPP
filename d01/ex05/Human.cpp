09b2d3cc40361ac143f9e4d7966eb377de17ce75
tvisenti <tvisenti@student.42.fr>
2017-04-05 13:54:42 +0200

    [ex05]: Private

--- d01/ex05/Human.cpp
+++ d01/ex05/Human.cpp
@@ -6,7 +6,7 @@
 /*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/03 17:07:56 by tvisenti          #+#    #+#             */
-/*   Updated: 2017/04/04 15:55:43 by tvisenti         ###   ########.fr       */
+/*   Updated: 2017/04/05 13:52:13 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ Human::~Human(void) {
 	return;
 }
 
-const Brain	&Human::getBrain(void) {
+const Brain	&Human::getBrain(void) const {
 	return (this->brain);
 }
 
-std::string Human::identify(void) {
+std::string Human::identify(void) const {
 	return (this->brain.identify());
 }
--- d01/ex05/Human.hpp
+++ d01/ex05/Human.hpp
@@ -6,7 +6,7 @@
 /*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/03 17:07:54 by tvisenti          #+#    #+#             */
-/*   Updated: 2017/04/04 17:16:45 by tvisenti         ###   ########.fr       */
+/*   Updated: 2017/04/05 13:54:28 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,12 @@ class Human {
 	public:
 		Human(void);
 		~Human(void);
-		const Brain brain;
 
-		const Brain	&getBrain(void);
-		std::string	identify(void);
+		const Brain	&getBrain(void) const;
+		std::string	identify(void) const;
+		
+	private:
+		const Brain brain;
 };
 
 #endif
