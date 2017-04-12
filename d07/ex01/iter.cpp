5746390a2a11328edeb273db174f43db9456474c
tvisenti <tvisenti@student.42.fr>
2017-04-12 17:52:14 +0200

    [ex01]: Template for func

--- d07/ex01/iter.cpp
+++ d07/ex01/iter.cpp
@@ -6,16 +6,11 @@
 /*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 16:18:16 by tvisenti          #+#    #+#             */
-/*   Updated: 2017/04/12 17:30:02 by tvisenti         ###   ########.fr       */
+/*   Updated: 2017/04/12 17:51:43 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-template <typename T>
-void	printer(T & i) {
-	std::cout << i << std::endl;
-}
 
 template <typename T>
 void	iter(T *arr, size_t length, void(*fn)(T &)) {
@@ -24,14 +19,31 @@ void	iter(T *arr, size_t length, void(*fn)(T &)) {
 	}
 }
 
+template <typename T>
+void	printer(T & i) {
+	std::cout << i << std::endl;
+}
+
+void	incrementInt(int &i) {
+	++i;
+}
+
 int		main() {
-	std::cout << "---------------------ARRAY-INT" << std::endl;
+	std::cout << "---------------------TEMP-INT" << std::endl;
 	int arrayInt[4] = {1, 2, 3, 4};
 
 	iter<int>(arrayInt, 4, &printer);
 
-	std::cout << "\n---------------------ARRAY-STR" << std::endl;
+	std::cout << "\n---------------------TEMP-STR" << std::endl;
 	std::string arrayStr[4] = {"Je", "suis", "un", "test"};
 	iter<std::string>(arrayStr, 4, &printer);
+
+	std::cout << "\n---------------------FUNC-INT" << std::endl;
+	int arrayIntFct[4] = {1, 2, 3, 4};
+
+	iter<int>(arrayIntFct, 4, &incrementInt);
+	for (int i = 0; i < 4; i++) {
+		std::cout << arrayIntFct[i] << '\n';
+	}
 	return (0);
 }
